3758c5bafbbaf0711bbfc337830538ccf727f3b7
ridolud <faridho.lutfi@gmail.com>
2019-04-24 23:06:04 +0700

    Initial Commit

---
+++ nyumbang/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  nyumbang
+//
+//  Created by Faridho Luedfi on 24/04/19.
+//  Copyright © 2019 Apple Developer Academy - Kelompok 6. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
