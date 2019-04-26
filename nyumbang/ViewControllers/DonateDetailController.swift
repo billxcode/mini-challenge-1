45acf8d35eefc1cdc82eb78e6e9354254d8afe46
ridolud <faridho.lutfi@gmail.com>
2019-04-26 19:53:00 +0700

    pasing data segue - donation details

---
+++ nyumbang/ViewControllers/DonateDetailController.swift
@@ -0,0 +1,24 @@
+//
+//  DonateDetailController.swift
+//  nyumbang
+//
+//  Created by Faridho Luedfi on 26/04/19.
+//  Copyright © 2019 Apple Developer Academy - Kelompok 6. All rights reserved.
+//
+
+import UIKit
+
+class DonateDetailController: UIViewController {
+
+    var donation: Donation!
+    
+    @IBOutlet weak var eventNameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        eventNameLabel.text = donation?.eventName
+    }
+    
+
+}
