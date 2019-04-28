a470ab7d63d9f0a14e1ab5afc9e393981567be0b
Bill Tanthowi Jauhari <bill.tanthowi.j@gmail.com>
2019-04-28 12:42:09 +0700

    success detail event and list event

--- nyumbang/ViewControllers/YayasanDetailViewController.swift
+++ nyumbang/ViewControllers/YayasanDetailViewController.swift
@@ -13,14 +13,14 @@ class YayasanDetailViewController: UIViewController {
     @IBOutlet weak var placeFoundation: UILabel!
     @IBOutlet weak var photoFoundation: UIImageView!
     @IBOutlet weak var bannerFoundation: UIImageView!
-    @IBOutlet weak var listEventFoundation: UITableView!
+    @IBOutlet weak var listEventTable: UITableView!
     
     var yayasan: Yayasan!
-    
-    var listEvent: [Yayasan] = []
+    var listEvent: [Event] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        listEvent = createEventArray()
         
         titleFoundation.text = yayasan?.title
         photoFoundation.image = yayasan?.photo
@@ -28,6 +28,26 @@ class YayasanDetailViewController: UIViewController {
         placeFoundation.text = yayasan?.place
         
         // Do any additional setup after loading the view.
+    }
+    
+    func createEventArray() -> [Event]{
+        let description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+        let place = "Tangerang"
+        
+        let event1 =  Event(title: "Penggalangan Barang Kantor Malang", place: place, description: description, thumbnail: UIImage(imageLiteralResourceName: "vlog-4.png"), timeRemaining: 14)
+        let event2 =  Event(title: "Penggalangan Barang Kantor Malang", place: place, description: description, thumbnail: UIImage(imageLiteralResourceName: "vlog-4.png"), timeRemaining: 14)
+        let event3 =  Event(title: "Penggalangan Barang Kantor Malang", place: place, description: description, thumbnail: UIImage(imageLiteralResourceName: "vlog-4.png"), timeRemaining: 14)
+        let event4 =  Event(title: "Penggalangan Barang Kantor Malang", place: place, description: description, thumbnail: UIImage(imageLiteralResourceName: "vlog-4.png"), timeRemaining: 14)
+        let event5 =  Event(title: "Penggalangan Barang Kantor Malang", place: place, description: description, thumbnail: UIImage(imageLiteralResourceName: "vlog-4.png"), timeRemaining: 14)
+        let event6 =  Event(title: "Penggalangan Barang Kantor Malang", place: place, description: description, thumbnail: UIImage(imageLiteralResourceName: "vlog-4.png"), timeRemaining: 14)
+        let event7 =  Event(title: "Penggalangan Barang Kantor Malang", place: place, description: description, thumbnail: UIImage(imageLiteralResourceName: "vlog-4.png"), timeRemaining: 14)
+        let event8 =  Event(title: "Penggalangan Barang Kantor Malang", place: place, description: description, thumbnail: UIImage(imageLiteralResourceName: "vlog-4.png"), timeRemaining: 14)
+        let event9 =  Event(title: "Penggalangan Barang Kantor Malang", place: place, description: description, thumbnail: UIImage(imageLiteralResourceName: "vlog-4.png"), timeRemaining: 14)
+        let event10 =  Event(title: "Penggalangan Barang Kantor Malang", place: place, description: description, thumbnail: UIImage(imageLiteralResourceName: "vlog-4.png"), timeRemaining: 14)
+        let event11 =  Event(title: "Penggalangan Barang Kantor Malang", place: place, description: description, thumbnail: UIImage(imageLiteralResourceName: "vlog-4.png"), timeRemaining: 14)
+        let event12 =  Event(title: "Penggalangan Barang Kantor Malang", place: place, description: description, thumbnail: UIImage(imageLiteralResourceName: "vlog-4.png"), timeRemaining: 14)
+        
+        return [event5, event2, event7, event3, event1, event6, event4, event8, event9, event10, event11, event12]
     }
     
 
@@ -43,30 +63,31 @@ class YayasanDetailViewController: UIViewController {
 
 }
 
-//extension YayasanDetailViewController: UITableViewDataSource, UITableViewDelegate {
-//
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return listEvent.count
-//    }
+extension YayasanDetailViewController: UITableViewDataSource, UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listEvent.count
+    }
 
     
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let yayasanKita = listEvent[indexPath.row]
-//        let cell = listEvent.dequeueReusableCell(withIdentifier: "EventFoundationCell") as! EventFoundationCell
-//
-//        return cell
-//    }
-//
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let yayasanKita = listEvent[indexPath.row]
+        let cell = listEventTable.dequeueReusableCell(withIdentifier: "EventFoundationCell") as! EventCell
+        cell.setEvent(event: yayasanKita)
+        
+        return cell
+    }
+
 //    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 //
-//        let indexPath = listEvent.indexPathForSelectedRow
-//        let currentDiscovery = listOfSearch.cellForRow(at: indexPath!) as! YayasanCell
+//        let indexPath = listEventTable.indexPathForSelectedRow
+//        let currentDiscovery = listEventTable.cellForRow(at: indexPath!) as! EventCell
 //
 //        guard let discoveryDetail = segue.destination as? YayasanDetailViewController else { return }
 //        discoveryDetail.yayasan = currentDiscovery.yayasan
 //
 //    }
     
-//}
+}
 
 
